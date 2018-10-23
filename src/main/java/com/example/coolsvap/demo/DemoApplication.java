13bdc827cbb67eb8be0661dcb58834e690ad6108
Swapnil Kulkarni <Swapnil_Kulkarni@Persistent.com>
2018-10-23 15:17:04 +0530

    Add Spring Boot Demo Application
    Signed-off-by: Swapnil Kulkarni <Swapnil_Kulkarni@Persistent.com>

---
+++ src/main/java/com/example/coolsvap/demo/DemoApplication.java
@@ -0,0 +1,31 @@
+package com.example.coolsvap.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class DemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+
+    @Value("${spring.application.name}")
+    private String appName;
+    @RequestMapping(value = "/")
+    public String hello(){
+        return appName;
+    }
+}
